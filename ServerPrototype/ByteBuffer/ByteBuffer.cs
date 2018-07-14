6d007617a802de1af22722b545f486b922587122
AGaudreau <40871526+AGaudreau@users.noreply.github.com>
2018-07-13 19:28:04 -0700

    ByteBuffer.dll intial implementation completed
    only debug was built for now

--- ServerPrototype/ByteBuffer/ByteBuffer.cs
+++ ServerPrototype/ByteBuffer/ByteBuffer.cs
@@ -2,18 +2,22 @@
 using System;
 using System.Text;
 
-public class ByteBuffer
+public class ByteBuffer: IDisposable
 {
+  // Member variables
   private List<byte> buffer;
   private byte[] readBuffer;
   private int readPosition;
   private bool bufferUpdated = false;
-  
+  private bool disposedValue = false; // detects redudant calls to dispose
+
+  // Construction
   public ByteBuffer() {
     buffer = new List<byte>();
     readPosition = 0;
   }
 
+  // utility functions
   public long getReadPosition() {
     return readPosition;
   }
@@ -31,6 +35,7 @@ public class ByteBuffer
     readPosition = 0;
   }
 
+  // Write methods
   public void writeBytes(byte[] input) {
     buffer.AddRange(input);
     bufferUpdated = true;
@@ -57,24 +62,106 @@ public class ByteBuffer
     bufferUpdated = true;
   }
 
+  // Read methods
   public int readInteger(bool peek = true) {
     if (buffer.Count > readPosition) {
       if (bufferUpdated) {
         readBuffer = buffer.ToArray();
         bufferUpdated = false;
       }
-      int ret = BitConverter.ToInt32(readBuffer, readPosition);
+      int returnValue = BitConverter.ToInt32(readBuffer, readPosition);
       if (peek && buffer.Count > readPosition) {
         readPosition += 4;
       }
-      return ret;
+      return returnValue;
+    } else {
+      throw new Exception("Byte buffer is past limit");
+    }
+  }
+  public byte[] readBytes(int length, bool peek = true) {
+    if (bufferUpdated) {
+      readBuffer = buffer.ToArray();
+      bufferUpdated = false;
+    }
+
+    byte[] returnValue = buffer.GetRange(readPosition, length).ToArray();
+    if (peek)
+      readPosition += length;
+
+    return returnValue;
+  }
+  public string readString(bool peek = true) {
+    int length = readInteger(true);
+    if (bufferUpdated) {
+      readBuffer = buffer.ToArray();
+      bufferUpdated = false;
+    }
+
+    string returnValue = Encoding.ASCII.GetString(readBuffer, readPosition, length);
+    if (peek && buffer.Count > readPosition && returnValue.Length > 0) {
+      readPosition += length;
+    }
+    return returnValue;
+  }
+  public short readShort(bool peek = true) {
+    if (buffer.Count > readPosition) {
+      if (bufferUpdated) {
+        readBuffer = buffer.ToArray();
+        bufferUpdated = false;
+      }
+      short returnValue = BitConverter.ToInt16(readBuffer, readPosition);
+      if (peek && buffer.Count > readPosition) {
+        readPosition += 2;
+      }
+      return returnValue;
+    } else {
+      throw new Exception("Byte buffer is past limit");
+    }
+  }
+  public float readFloat(bool peek = true) {
+    if (buffer.Count > readPosition) {
+      if (bufferUpdated) {
+        readBuffer = buffer.ToArray();
+        bufferUpdated = false;
+      }
+      float returnValue = BitConverter.ToSingle(readBuffer, readPosition);
+      if (peek && buffer.Count > readPosition) {
+        readPosition += 4;
+      }
+      return returnValue;
+    } else {
+      throw new Exception("Byte buffer is past limit");
+    }
+  }
+  public long readLong(bool peek = true) {
+    if (buffer.Count > readPosition) {
+      if (bufferUpdated) {
+        readBuffer = buffer.ToArray();
+        bufferUpdated = false;
+      }
+      long returnValue = BitConverter.ToInt64(readBuffer, readPosition);
+      if (peek && buffer.Count > readPosition) {
+        readPosition += 8;
+      }
+      return returnValue;
     } else {
       throw new Exception("Byte buffer is past limit");
     }
   }
 
-  public byte[] readBytes(int length, bool peek = true) {
+  // IDisposable methods
+  protected virtual void dispose(bool disposing) {
+    if (!disposedValue) {
+      if (disposing) {
+        buffer.Clear();
+      }
+      readPosition = 0;
+    }
 
+    disposedValue = true;
   }
-
+  public void Dispose() {
+    dispose(true);
+    GC.SuppressFinalize(this);
+  }
 }
