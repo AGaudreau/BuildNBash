4b5bd6620d5719998167b7b3a1dda3b8d31aa007
vannesa-farmer <28429147+vannesa-farmer@users.noreply.github.com>
2019-05-31 21:17:55 -0700

    Tablet interaction works to an extent

---
+++ MainProject/Assets/TabletInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabletInteraction : MonoBehaviour
+{
+  public Camera tabletCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetMouseButtonDown(0)) {
+        RaycastHit hit;
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+      if (Physics.Raycast(ray, out hit)) {
+          Debug.Log(hit.collider.gameObject);
+
+          var localPoint = hit.textureCoord;
+          Ray tabletRay = tabletCamera.ViewportPointToRay(hit.textureCoord);
+          RaycastHit tabletHit;
+
+          if (Physics.Raycast(tabletRay, out tabletHit)) {
+            Debug.Log(tabletHit.collider.gameObject.name);
+          }
+
+        }
+      }
+    }
+}
