c181ecc656385522c43cbc9195b94aea8212cff7
AGaudreau <40871526+AGaudreau@users.noreply.github.com>
2019-06-08 17:12:50 -0700

    Added interaction testing scene
    Can drag a block around sorta.

---
+++ MainProject/Assets/PlayerInteraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+  public Camera playerCamera;
+  GameObject grabbedBlock;
+  bool grabObject = false;
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  
+    if (Input.GetMouseButtonDown(0)) {
+      if (grabObject) {
+        grabObject = false;
+      } else {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit racastResults;
+        if (Physics.Raycast(ray, out racastResults) && racastResults.transform.parent) {
+          
+          string nameOfThingWeClickedOn = racastResults.transform.parent.gameObject.name;
+          if (nameOfThingWeClickedOn.Contains("Block")) {
+            grabbedBlock = racastResults.transform.parent.gameObject;
+            grabObject = true;
+            Debug.Log("Grabbed block");
+          }
+        }
+      }
+    }
+
+    if (grabObject) {
+      grabbedBlock.transform.position = playerCamera.transform.position + playerCamera.transform.forward * 1;
+    }
+
+  }
+}
---
+++ MainProject/Assets/dragScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragScript : MonoBehaviour
+{
+  public float reach = 2;
+
+  private void OnMouseDrag() {
+    Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, reach);
+    Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    transform.position = objPosition;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+}
